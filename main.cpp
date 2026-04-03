e10ae056db1ea65cba0cd49606f03ec2d400d160
Antohi Robert <antohir@gmail.com>
2026-04-03 22:58:26 +0300

    fixed -c
    # Conflicts:
    #	README.md

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+//#include <cstddef>
 import AntohiRobert_counter2;
 //import AntohiRobert_counter;
 import AntohiRobert_binpow;
+
 int main (void)
 {
   AntohiRobert_counter2::counter();
   //AntohiRobert_counter::counter();
-  int ans = AntohiRobert_binpow::binpow(2,5,79);
+  long long ans = AntohiRobert_binpow::binpow(2,5,79);
   std::cout<<ans<<std::endl;
   return 0;
 }
